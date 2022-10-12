1536a42b8e4b838f6d8711382b7316c6d2191a3c
CryptoSax <joshua@operatorfoundation.org>
2022-10-12 06:56:46 -0500

    removed submodule

---
+++ MinimalVPNApp/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  MinimalVPNApp
+//
+//  Created by Joshua Clark on 10/11/22.
+//
+
+import SwiftUI
+import MinimalVPNLibrary
+
+struct ContentView: View
+{
+    let vpn = VPNManager()
+
+    var body: some View
+    {
+        Text("🫧 MinimalVPN 🫧")
+        Button("Connect")
+        {
+            self.vpn.enable()
+            do
+            {
+                try self.vpn.manager.connection.startVPNTunnel()
+            }
+            catch
+            {
+                print("Failed to start the tunnel: \(error)")
+            }
+        }
+        
+        Button("Disconnect")
+        {
+            self.vpn.manager.connection.stopVPNTunnel()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ MinimalVPNApp/MinimalVPNApp.swift
@@ -0,0 +1,18 @@
+//
+//  MinimalVPNAppApp.swift
+//  MinimalVPNApp
+//
+//  Created by Joshua Clark on 10/11/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct MinimalVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
